5d4f2ba47e9f27b772868f93d99be3f3e6d0e459
Wellington Pereira <wellington.costa128@gmail.com>
2022-05-20 14:48:25 -0300

    feat: define domain entities

---
+++ src/main/java/br/com/aegro/datasync/domain/entity/User.java
@@ -0,0 +1,62 @@
+package br.com.aegro.datasync.domain.entity;
+
+import java.util.Objects;
+
+public class User {
+
+    private String id;
+    private String fullName;
+    private String email;
+
+    public User(String id, String fullName, String email) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) && fullName.equals(user.fullName) && email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
