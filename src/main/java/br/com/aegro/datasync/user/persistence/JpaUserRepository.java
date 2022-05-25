e84970ab24f80ff25670af74ee739e172870877d
Wellington Pereira <wellington.costa128@gmail.com>
2022-05-25 16:24:29 -0300

    feat: implement the UserRepository

---
+++ src/main/java/br/com/aegro/datasync/user/persistence/JpaUserRepository.java
@@ -0,0 +1,30 @@
+package br.com.aegro.datasync.user.persistence;
+
+import br.com.aegro.datasync.user.domain.User;
+import br.com.aegro.datasync.user.domain.UserRepository;
+import br.com.aegro.datasync.user.persistence.dao.UserDao;
+import br.com.aegro.datasync.user.persistence.mapper.UserMapper;
+
+import java.util.Optional;
+
+public class JpaUserRepository implements UserRepository {
+
+    private final UserDao userDao;
+    private final UserMapper userMapper;
+
+    public JpaUserRepository(UserDao userDao, UserMapper userMapper) {
+        this.userDao = userDao;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userDao.findByEmail(email).map(userMapper::toDomain);
+    }
+
+    @Override
+    public User save(User user) {
+        var savedUser = userDao.save(userMapper.toData(user));
+        return userMapper.toDomain(savedUser);
+    }
+}
