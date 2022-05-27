5f79a58958a0ae790b8792b2470f5ba6e499838d
Wellington Pereira <wellington.costa128@gmail.com>
2022-05-27 17:39:59 -0300

    feat: implement the endpoint for saving seeds

--- src/main/java/br/com/aegro/datasync/edge/rest/SeedRestController.java
+++ src/main/java/br/com/aegro/datasync/edge/rest/SeedRestController.java
@@ -4,11 +4,18 @@ import br.com.aegro.datasync.edge.rest.resources.SeedResource;
 import br.com.aegro.datasync.edge.rest.resources.converter.SeedResourceConverter;
 import br.com.aegro.datasync.seed.application.SeedApplicationService;
 import br.com.aegro.datasync.seed.application.SeedQueryApplicationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+import javax.validation.ValidationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,5 +44,16 @@ public class SeedRestController {
         return seedQueryApplicationService.findAllByUser(userId)
                 .stream().map(seedResourceConverter::convertTo)
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@Valid @RequestBody SeedResource seedResource) {
+        try {
+            var seedModel = seedResourceConverter.convertFrom(seedResource);
+            seedApplicationService.save(seedModel);
+        } catch (ValidationException validationException) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, null, validationException);
+        }
     }
 }
