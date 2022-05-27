33a98dd02896944024cb6161189dd2741618ea16
Wellington Pereira <wellington.costa128@gmail.com>
2022-05-27 15:49:55 -0300

    feat: implement a domain service to save seeds

---
+++ src/main/java/br/com/aegro/datasync/seed/domain/SeedRegistryDomainService.java
@@ -0,0 +1,9 @@
+package br.com.aegro.datasync.seed.domain;
+
+import br.com.aegro.datasync.seed.domain.model.Seed;
+
+public interface SeedRegistryDomainService {
+
+    Seed register(Seed seed);
+
+}
